b8999692601ebb793daccb9bd47294af5b692972
urban.marton <urban.marton@gmail.com>
2021-01-25 23:16:49 +0100

    Back to "3 rules merged into one" commit + added input + simplified possible_digits_left list creation.

---
+++ Exercises/Lock Combination/README.md
@@ -0,0 +1,1 @@
+![screenshot](lock_guess.jpg)
--- Exercises/Lock Combination/lock.py
+++ Exercises/Lock Combination/lock.py
@@ -1,21 +1,19 @@
-wrong_digits = str(523)
-one_digit_right = str(189)
-one_digit_wrong1 = str(147)
-one_digit_wrong2 = str(286)
-two_correct = str(964)
+one_digit_wrong1 = input("One digit is right but in the wrong place (example input: 147): ")
+one_digit_right = input("One digit is right and in its place (example input: 189): ")
+two_correct = input("Two digits are correct but both are in the wrong place (example input: 964): ")
+wrong_digits = input("All digit are wrong (example input: 523): ")
+one_digit_wrong2 = input("One digit is right but in the wrong place (example input: 286): ")
 one_digit_wrong = [one_digit_wrong1, one_digit_wrong2, two_correct]
 
-all_digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 one_possible_comb = []
 possible_combs = []
 
-possible_digits_left = [digit for digit in range(10) if
-                        str(digit) != one_digit_right[0] and
-                        str(digit) != one_digit_right[1] and
-                        str(digit) != one_digit_right[2] and
-                        str(digit) != wrong_digits[0] and
-                        str(digit) != wrong_digits[1] and
-                        str(digit) != wrong_digits[2]]
+"""Rule 4 and initialization for rule 2:
+Remove the digits of rule 2 as well, because they will be referenced directly using 
+one_digit_right[0], one_digit_right[1], and one_digit_right[2] in the next loop."""
+possible_digits_left = [digit for digit in range(10) if str(digit) not in wrong_digits and
+                                                        str(digit) not in one_digit_right]
+
 
 """One digit is right and its place (first digit)"""
 one_possible_comb.append(int(one_digit_right[0]))
@@ -56,7 +54,9 @@ for digit in possible_digits_left:
             one_possible_comb.pop()
 # +12 possiblity = 36 left
 
-"""One digit is right but in the wrong place (for all 3 rules)"""
+"""For rules 1, 3 and 5.
+"two digits are correct but both are in the wrong place" =
+"one digit is right but in the wrong place", the 5 possible permutations are identical)"""
 for rule in one_digit_wrong:
     for place in range(3):
         [possible_combs.remove(possible_combination) for possible_combination in possible_combs.copy()
