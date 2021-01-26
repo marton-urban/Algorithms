f8f766cbb1bdaf01d8f2cdae00ddb684e22edeea
urban.marton <urban.marton@gmail.com>
2021-01-26 15:44:36 +0100

    Working and fast, but doesn't give all answers (occurences list gets filled by the lower loops quickly).

--- Exercises/Lock Combination/lock_recursive.py
+++ Exercises/Lock Combination/lock_recursive.py
@@ -25,55 +25,81 @@ def current_time(digit, name):
                                                                             one_digit_wrong2,
                                                                             name))
 
-
+occurences = []
 for digit01 in range(1, 10):
+    occurences.clear()
+    if occurences.count(digit01) == 2:
+        continue
+    occurences.append(digit01)
     current_time(digit01, 'Digit01')
     for digit02 in range(1, 10):
-        if digit02 == digit01:
+        if digit02 == digit01 or occurences.count(digit02) == 2:
             continue
+        occurences.append(digit02)
         current_time(digit02, 'Digit02')
         for digit03 in range(1, 10):
-            if digit03 == digit02 or digit03 == digit01:
+            if digit03 == digit02 or digit03 == digit01 or occurences.count(digit03) == 2:
                 continue
+            occurences.append(digit03)
             current_time(digit03, 'Digit03')
             one_digit_wrong1 = f"{digit01}{digit02}{digit03}"
             for digit04 in range(1, 10):
+                if occurences.count(digit04) == 2:
+                    continue
+                occurences.append(digit04)
                 current_time(digit04, 'Digit04')
                 for digit05 in range(1, 10):
-                    if digit05 == digit04:
+                    if digit05 == digit04 or occurences.count(digit05) == 2:
                         continue
+                    occurences.append(digit05)
                     current_time(digit05, 'Digit05')
                     for digit06 in range(1, 10):
-                        if digit06 == digit05 or digit06 == digit04:
+                        if digit06 == digit05 or digit06 == digit04 or occurences.count(digit06) == 2:
                             continue
+                        occurences.append(digit06)
                         current_time(digit06, 'Digit06')
                         one_digit_right = f"{digit04}{digit05}{digit06}"
                         for digit07 in range(1, 10):
+                            if occurences.count(digit07) == 2:
+                                continue
+                            occurences.append(digit07)
                             current_time(digit07, 'Digit07')
                             for digit08 in range(1, 10):
-                                if digit08 == digit07:
+                                if digit08 == digit07 or occurences.count(digit08) == 2:
                                     continue
+                                occurences.append(digit08)
                                 current_time(digit08, 'Digit08')
                                 for digit09 in range(1, 10):
-                                    if digit09 == digit08 or digit09 == digit07:
+                                    if digit09 == digit08 or digit09 == digit07 or occurences.count(digit09) == 2:
                                         continue
+                                    occurences.append(digit09)
                                     current_time(digit09, 'Digit09')
                                     two_correct = f"{digit07}{digit08}{digit09}"
                                     for digit10 in range(1, 10):
+                                        if occurences.count(digit10) == 2:
+                                            continue
+                                        occurences.append(digit10)
                                         for digit11 in range(1, 10):
-                                            if digit11 == digit10:
+                                            if digit11 == digit10 or occurences.count(digit11) == 2:
                                                 continue
+                                            occurences.append(digit11)
                                             for digit12 in range(1, 10):
-                                                if digit12 == digit11 or digit12 == digit10:
+                                                if digit12 == digit11 or digit12 == digit10 or occurences.count(digit12) == 2:
                                                     continue
+                                                occurences.append(digit12)
                                                 wrong_digits = f"{digit10}{digit11}{digit12}"
                                                 for digit13 in range(1, 10):
+                                                    if occurences.count(digit13) == 2:
+                                                        continue
+                                                    occurences.append(digit13)
                                                     for digit14 in range(1, 10):
-                                                        if digit14 == digit13:
+                                                        if digit14 == digit13 or occurences.count(digit14) == 2:
                                                             continue
+                                                        occurences.append(digit14)
                                                         for digit15 in range(1, 10):
-                                                            if digit15 == digit14 or digit15 == digit13:
+                                                            if digit15 == digit14 or digit15 == digit13  or occurences.count(digit15) == 2:
                                                                 continue
+                                                            occurences.append(digit15)
                                                             one_digit_wrong2 = f"{digit13}{digit14}{digit15}"
                                                             one_digit_wrong = [one_digit_wrong1, one_digit_wrong2,
                                                                                two_correct]
@@ -86,7 +112,7 @@ for digit01 in range(1, 10):
                                                             because they will be referenced directly using 
                                                             one_digit_right[0], one_digit_right[1], 
                                                             and one_digit_right[2] in the next loop."""
-                                                            possible_digits_left = [str(digit) for digit in range(10) if
+                                                            possible_digits_left = [str(digit) for digit in range(1, 10) if
                                                                                     str(digit) not in wrong_digits and
                                                                                     str(digit) not in one_digit_right]
 
